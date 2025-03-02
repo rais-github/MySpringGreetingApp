fe24ecf5703a4b5beae872162836ab5169e03a7b
Himanshu Rai <himanshu2003rai@gmail.com>
2025-03-02 13:26:39 +0530

    Initialise Greeting Project

---
+++ src/main/java/com/MyGreetingApp/backend/MyGreetingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.MyGreetingApp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGreetingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGreetingProjectApplication.class, args);
+	}
+
+}
